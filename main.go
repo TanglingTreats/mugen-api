5e121b2c7cd0a77caa8fdc907ade8050633d67ec
Edwin Tang <edwintang10@gmail.com>
2024-04-02 09:39:07 +0800

    feat(endpoint): add challenge endpoint for query

--- main.go
+++ main.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"fmt"
-	_ "github.com/gin-gonic/gin"
-	_ "net/http"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
+type challenge struct {
+	Data string `json:"data"`
+}
+
+var text = "crypto decentralized meme stock stonk hodl ape GameStop AMC Reddit Robinhood Dogecoin elon tesla Twitter Muskrat quiet quitting great resignation quiet firing layoff recession inflation cost of living supply chain chip shortage climate crisis heat wave drought fire season net zero green energy EV plant-based oat milk cauliflower gnocchi charcuterie grazing board cheugy cringe slay zaddy bussy thirst trap y'all cap no cap fr fr wig go off understood the assignment hot girl walk feral girl summer that's the tweet main character energy unalive sadfishing negging love-bombing gatekeeping cloutlighting sliving going goblin mode crisitunity ambient anxiety"
+
 func main() {
-	fmt.Println("hello")
+	fmt.Println("Starting RESTful service")
+	router := gin.Default()
+	router.GET("/challenges", getChallenge)
+
+	router.Run(":8080")
+}
+
+func getChallenge(c *gin.Context) {
+	res := challenge{
+		Data: text,
+	}
+	c.IndentedJSON(http.StatusOK, res)
 }
